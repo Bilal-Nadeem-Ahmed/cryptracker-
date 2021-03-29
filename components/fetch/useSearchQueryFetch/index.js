39591bbdb0f7e9e8e50ee565a087e0966ef74179
Bilal Ahmed <bilal91_@hotmail.com>
2021-03-29 15:44:41 +0100

    changed naming

---
+++ components/fetch/useSearchQueryFetch/index.js
@@ -0,0 +1,6 @@
+const useSearchQueryFetch = (val, setSearch) => {
+  const data = ' this is the searchqueryfetch.'
+  setSearch(val)
+}
+
+export default useSearchQueryFetch
--- components/search/subcomponents/searchBar/index.js
+++ components/search/subcomponents/searchBar/index.js
@@ -1,8 +1,12 @@
+import useSearchQueryFetch from '../../../fetch/useSearchQueryFetch'
+
 const SearchBar = ({ setSearch, item }) => {
   const handleChange = (e) => {
     let newVal = e.target.value
-    setSearch(newVal)
+    // this is the hook i made for the fetch. it currently takers the input and a function to set a value,  this is all that is currently needed for this fetch. next step is to rename it as a hook. and then look at more deatil at the available apis.
+    useSearchQueryFetch(newVal, setSearch)
   }
+
   return (
     <div>
       <input onChange={(e) => handleChange(e)} type="text" />
