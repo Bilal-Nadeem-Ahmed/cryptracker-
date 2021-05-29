da3d5a0eaf2fb95f6f1c2b075275d7a22f1332d9
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-29 19:46:35 +0100

    changed nav

---
+++ components/common/Navbar/index.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return ( 
+    <div className='navbar'>
+      <ul>
+        <li>Cryptracker</li>
+        <li>About</li>
+        <li>Search</li>
+      </ul>
+    </div>
+  )
+}
+ 
+export default Navbar
